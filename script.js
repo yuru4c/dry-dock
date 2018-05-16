122145070f3dd58e42f5a3456ad1f8ca3862dabb
ゆる <yuru4c@gmail.com>
2018-05-16 18:39:31 +0900

    軽微な修正
    - 初期状態の Main をシリアライズできないバグを修正
    - リファクタリング

--- script.js
+++ script.js
@@ -414,9 +414,8 @@ var Draggable = _(function (Base, base) {
 		function mousedown(event) {
 			var target = event.target;
 			if (target == this || target == self.body) {
-				if (event.button) {
-					self.activate();
-				} else {
+				if (event.button) self.activate();
+				else {
 					var container = self.getContainer();
 					container.setDragging(self);
 					container.mousedown(event);
@@ -1219,10 +1218,12 @@ var Mutable = _(function (Base, base) {
 		}
 		this.body.removeChild(child.element);
 		
-		if (length && child == this.active) {
-			var index = child.index;
-			if (index == length) index = length - 1;
-			this.activateChild(this.children[index]);
+		if (child == this.active) {
+			if (length) {
+				var index = child.index;
+				if (index == length) index = length - 1;
+				this.activateChild(this.children[index]);
+			} else this.active = null;
 		}
 	};
 	prototype.replaceChild = function (child, oldChild) {
@@ -2144,13 +2145,15 @@ var Main = _(function (Base, base) {
 	prototype.toJSON = function () {
 		var children = []; var active = 0;
 		
-		var index = this.active.index;
-		var length = this.children.length;
-		for (var i = 0; i < length; i++) {
-			var child = this.children[i];
-			if (child.id) {
-				children.push(child);
-				if (i < index) active++;
+		if (this.active) {
+			var index = this.active.index;
+			var length = this.children.length;
+			for (var i = 0; i < length; i++) {
+				var child = this.children[i];
+				if (child.id) {
+					children.push(child);
+					if (i < index) active++;
+				}
 			}
 		}
 		if (active == children.length) {
